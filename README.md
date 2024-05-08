6af98a269778f5e0c0b5d11076c15306afe05109
tiagosora <tiagogcarvalho2002.@>
2024-05-08 22:41:24 +0100

    readme update

--- README.md
+++ README.md
@@ -1,13 +1,44 @@
-## TAI Lab2
+# TAI_Lab2 - GUIDE
 
-### Input args:
+## How to run the isChatted program
 
-- 1. ChatGPT prompt directory
-- 2. Human prompt directory
-- 3. Text to be analysed
-- 4. Window size (k)
-- 5. Smoothing factor (a)
+Before running the program, the user must have the following requirements.
 
-Mapa:
-chave -> k characters
-value -> alphabet letter
+1. Create/Be on a virutal environment;
+
+2. Installed the requirements inside `requirements.txt`
+
+    ```bash
+    pip install requirements.txt
+    ```
+
+3. Have the models inside the `./models/` folder. You can download the models from https://uapt33090-my.sharepoint.com/:u:/g/personal/tiagogcarvalho_ua_pt/EbwiCwn5H0BCmRkFVvFKRnkBBNkepXtmktnRC0SlspvHbg?e=P9HSap
+
+
+Upon concluding the installation phase, you may run the program following these steps:
+
+1. Compile the file
+
+    ```bash
+    g++ -o bin/chatted main.cpp -std=c++17
+    ```
+
+2. Execute file with arguments, *assumining you have the chry.txt file inside example folder*:
+
+    ```bash
+    ./bin/chatted -g ./models/gpt-4/gpt_files/ -h ./models/gpt-4/human_files/ -t ./input_files/input_file.txt -f nsec -k 10 -a 0.5 -s 100 -o ./results_file.txt -b Human
+    ```
+
+    where `-g ./models/gpt-4/gpt_files/ -h ./models/gpt-4/human_files/ -t ./input_files/input_file.txt -f nsec -k 10 -a 0.5 -s 100 -o ./results_file.txt -b Human` are the arguments that need to be passed.
+
+    Alternatively, you can ommit the `g`, which, by default, is equal to `./models/gpt-4/gpt_files/`; `h`, by default `./models/gpt-4/human_files/`; `t`, by default `./test_files/test_gpt_files/`; `k`, by default `3`; `a`, by default `0.5`; `s`, by default `5000`.
+
+## Additional experiment
+
+We also created a interface that helps the user on running the program.
+
+To run the interface the user must conclude the installation phase detailed on the top and run the following comand:
+
+```bash
+python app.py
+```
--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import customtkinter as ctk
 import os
 import subprocess
-
 from tkinter import filedialog
 
 ctk.set_appearance_mode("light")
 
+
 def process_inputs():
+    """
+    Process inputs from the GUI and execute a command to perform a task.
+
+    :return: None
+    """
     try:
         # Disable the button to prevent multiple submissions
         process_button.configure(state=ctk.DISABLED)
-        
+
         # Update the status label to indicate processing
         status_label.configure(text="Processing... Please wait.")
         root.update_idletasks()
-        
+
         # Collect parameters from the GUI
         filters = []
         if exclude_numbers.get(): filters.append("n")
@@ -23,27 +28,26 @@ def process_inputs():
         if exclude_case.get(): filters.append("c")
         filters_str = ''.join(filters)
 
-
         model_values = {"GPT 4": "gpt-4", "GPT 3.5": "gpt-3.5", "Both": "merge"}
         model = model_values[model_var.get()]
         alpha = alpha_slider.get()
         k = k_entry.get()
-        sizeLimit = sizeLimit_entry.get()
-        
-        inputDir = './input_files/'
-        if not os.path.exists(inputDir):
-            os.makedirs(inputDir)
-            
-        resultsDir = "./results/"
-        if not os.path.exists(resultsDir):
-            os.makedirs(resultsDir)
-            
-        with open(f"./{inputDir}/input_file.txt", "w") as file:
+        size_limit = sizeLimit_entry.get()
+
+        input_dir = './input_files/'
+        if not os.path.exists(input_dir):
+            os.makedirs(input_dir)
+
+        results_dir = "./results/"
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+
+        with open(f"./{input_dir}/input_file.txt", "w") as file:
             file.write(input_text.get("1.0", "end-1c"))
-            
-        with open(f"./{resultsDir}/results_file.txt", "w") as file:
+
+        with open(f"./{results_dir}/results_file.txt", "w") as file:
             pass
-        
+
         model_path_gpt = f"./models/{model}/gpt_files/"
         model_path_human = f"./models/{model}/human_files/"
         results_file_path = "./results_file.txt"
@@ -54,7 +58,7 @@ def process_inputs():
             "./run_interface.sh",
             "-g", model_path_gpt,
             "-h", model_path_human,
-            "-t", inputDir,
+            "-t", input_dir,
         ]
         if filters_str != "":
             command.append("-f")
@@ -65,31 +69,30 @@ def process_inputs():
         command += [
             "-a", str(alpha),
         ]
-        if str(sizeLimit) != "":
+        if str(size_limit) != "":
             command.append("-s")
-            command.append(sizeLimit)
+            command.append(size_limit)
         command += [
             "-o", results_file_path,
             "-b", gpt_human
         ]
-        
+
         # Execute the command
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         _ = process.communicate()
-        
 
         if process.returncode == 0:
             # Read the results from the specified results file
-            with open(resultsDir + results_file_path, "r") as file:
+            with open(results_dir + results_file_path, "r") as file:
                 results_content = file.read()
-                
+
             # Parse the results
             results_list = results_content.split(',')
             # is_gpt = results_list[3]
             is_human = results_list[5]
             bit_ratio = results_list[6]
             execution_time = results_list[8]
-            
+
             # Display the results in the results_frame
             result_text.configure(state=ctk.NORMAL)
             result_text.delete(1.0, ctk.END)
@@ -99,18 +102,19 @@ def process_inputs():
                 result_text.insert(ctk.END, "GPT")
             bit_ratio_text.delete(1.0, ctk.END)
             result_text.configure(state=ctk.DISABLED)
-            
+
             bit_ratio_text.configure(state=ctk.NORMAL)
             bit_ratio_text.delete(1.0, ctk.END)
             bit_ratio_text.insert(ctk.END, f"{bit_ratio}")
             bit_ratio_text.configure(state=ctk.DISABLED)
-            
+
             execution_time_text.configure(state=ctk.NORMAL)
             execution_time_text.delete(1.0, ctk.END)
             execution_time_text.insert(ctk.END, f"{execution_time}")
             execution_time_text.configure(state=ctk.DISABLED)
-            
+
         status_label.configure(text="Concluded. Program's Ready")
+
     except Exception as e:
         print("Error", str(e))
         status_label.configure(text="Error verifying. Program's Ready")
@@ -119,32 +123,46 @@ def process_inputs():
         result_text.insert(ctk.END, "")
         bit_ratio_text.delete(1.0, ctk.END)
         result_text.configure(state=ctk.DISABLED)
-        
+
         bit_ratio_text.configure(state=ctk.NORMAL)
         bit_ratio_text.delete(1.0, ctk.END)
         bit_ratio_text.insert(ctk.END, "")
         bit_ratio_text.configure(state=ctk.DISABLED)
-        
+
         execution_time_text.configure(state=ctk.NORMAL)
         execution_time_text.delete(1.0, ctk.END)
         execution_time_text.insert(ctk.END, "")
         execution_time_text.configure(state=ctk.DISABLED)
+
     finally:
         # Re-enable the button and update tasks regardless of the outcome
         process_button.configure(state=ctk.NORMAL)
-        
+
         root.update_idletasks()
 
+
 def toggle_theme():
+    """
+    Toggles the theme based on the switch state.
+
+    :return: None
+    """
     # Directly toggle the theme based on the switch state
     if theme_switch.get() == 1:
         ctk.set_appearance_mode("dark")
     else:
         ctk.set_appearance_mode("light")
 
+
 def open_file_dialog():
+    """
+    This function opens a file dialog and updates the `input_text` with the content of the selected file.
+
+    :return: None
+    """
     # This function opens a file dialog and updates the input_text with the content of the selected file
-    file_path = filedialog.askopenfilename(title="Select a file", filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
+    file_path = filedialog.askopenfilename(title="Select a file",
+                                           filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
     if file_path:
         try:
             with open(file_path, 'r') as file:
@@ -156,8 +174,9 @@ def open_file_dialog():
         finally:
             root.update_idletasks()
 
+
 root = ctk.CTk()
-root.title("GPT Detetor")
+root.title("GPT Detector")
 root.geometry("1000x550")
 
 # Configure column weights
@@ -279,10 +298,10 @@ execution_time_label = ctk.CTkLabel(execution_time_frame, text="Execution Time")
 execution_time_label.pack(side="top", padx=10, pady=(10, 5))
 execution_time_text = ctk.CTkTextbox(execution_time_frame, height=2, width=50, state=ctk.DISABLED)
 execution_time_text.pack(side="top", fill="both", padx=10, pady=(0, 10))
-        
+
 # Theme toggle frame at the bottom center
 theme_frame = ctk.CTkFrame(root, corner_radius=10)
-theme_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=(0,10), sticky="ws")
+theme_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=(0, 10), sticky="ws")
 
 # Theme toggle switch
 theme_switch = ctk.CTkSwitch(theme_frame, text="Dark Mode", command=toggle_theme)
